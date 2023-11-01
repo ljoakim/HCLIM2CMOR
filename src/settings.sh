047593d8052a2c82e84b75812c74a9f8731dfaf9
doblerone <andreasd@met.no>
2023-11-01 09:59:29 +0100

    Some small changes/comments

--- src/settings.sh
+++ src/settings.sh
@@ -14,6 +14,7 @@ EXP=evaluation     # driving experiment name
 NAMETAG=EUR11_EUR11_ALADIN43_v1_ERA5_r1i1p1f1_eval # file nameing of the input files, i.e. var_NAMETAG_date.nc
 CONSTANT_FOLDER=1950
 
+# Overwritten in case the wrapper is used
 SIMULATION=${OVERRIDE_SIMULATION:-${SIMULATION}}
 GCM=${OVERRIDE_GCM:-${GCM}}
 EXP=${OVERRIDE_EXP:-${EXP}}
@@ -22,21 +23,20 @@ CONSTANT_FOLDER=${OVERRIDE_CONSTANT_FOLDER:-${CONSTANT_FOLDER}}
 
 #-------------------------------------------
 # Directory path settings
-
 export BASEDIR=${HOME}/dev/repos/HCLIM2CMOR    # directory where the scripts are placed 
 export DATADIR=${HCLIMDIR}/postprocess/HCLIM2CMOR/data       # directory where all the data will be placed (typically at /scratch/)
 
 # scripts directory
-SRCDIR=${BASEDIR}/src                # directory where the post processing scripts are stored
+SRCDIR=${BASEDIR}/src                 # directory where the post processing scripts are stored
 SRCDIR_POST=${BASEDIR}/src/hclim_post # directory where the subscripts are stored
 
 WORKDIR=${DATADIR}/work/post/${GCM}_${EXP} # work directory
-LOGDIR=${DATADIR}/work/logs         # logging directory
+LOGDIR=${DATADIR}/work/logs                # logging directory
 
-# input/output directories and name of files
-INDIR_BASE=${HCLIMDIR}/${SIMULATION}/run/archive           # base where the input is located (in sub-directories YYYY/MM/01/00)
+# input/output directories
+INDIR_BASE=${HCLIMDIR}/${SIMULATION}/run/archive                         # base where the input is located (in sub-directories YYYY/MM/01/00)
 FXDIR=${HCLIMDIR}/${SIMULATION}/run/archive/${CONSTANT_FOLDER}/08/01/00/ # location of constant files (orog etc.)
-OUTDIR=${DATADIR}/work/input_CMORlight/${NAMETAG}                                                 # output directory for the annual files (to be used as input to CMOR python tool)
+OUTDIR=${DATADIR}/work/input_CMORlight/${NAMETAG}                        # output directory for the annual files (to be used as input to CMOR python tool)
 
 #-------------------------------------------
 # Other settings
